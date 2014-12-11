ebcb17924adf8c89f367bd825510710bd3247f55
William Wolf <throughnothing@gmail.com>
2014-12-11 12:42:45 -0800

    Fix?

--- lib/TAP/Formatter/JUnit/Session.pm
+++ lib/TAP/Formatter/JUnit/Session.pm
@@ -161,7 +161,7 @@ sub close_test {
 
     # track time for teardown, if needed
     if ($timer_enabled) {
-        my $duration = $self->parser->end_time - $queue->[-1]->time;
+        my $duration = eval { $self->parser->end_time - $queue->[-1]->time } || 0;
         my $case     = $xml->testcase( {
             'name' => _squeaky_clean('(teardown)'),
             'time' => $duration,
